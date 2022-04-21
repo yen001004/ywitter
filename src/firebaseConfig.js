acaad241cbab500d4b3f0a1684e148a7cc10b00f
이예원 <yenn001004@gmail.com>
2022-04-21 16:56:57 +0900

    Database Setup

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,5 +1,7 @@
-import { initializeApp } from "firebase/app";
+import * as firebase from "firebase/app";
 import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
+
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -9,5 +11,9 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_APP_ID
   };
 
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+export const firebaseInstance = firebase;
+
 export const authService = getAuth();
+export const dbService = getFirestore();
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,35 @@
-import React from "react";
+import { dbService } from "firebaseConfig";
+import React, { useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+    const [yweet, setYweet] = useState("");
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const docRef = await addDoc(collection(dbService, "yweets"), {
+                yweet,
+                createdAt: Date.now(),
+            });
+            console.log("Document written with ID: ", docRef.id);
+        } catch (error) {
+            console.error("Error adding document: ", error);
+        }
+        setYweet("");
+    };
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setYweet(value);
+    };
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input value={yweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+                <input type="submit" value="Yweet" />
+            </form>
+        </div>
+    );
+};
 export default Home;
