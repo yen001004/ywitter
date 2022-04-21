752bfe6e1fc788438a75899f92f7dadcef82d091
이예원 <yen@iyewon-ui-MacBookPro.local>
2022-04-21 14:02:19 +0900

    Router Setup

---
+++ src/components/Router.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+
+const AppRouter = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    return (
+        <Router>
+            <Routes>
+                {isLoggedIn ? (
+                <>
+                <Route exact path="/" element={<Home />}>
+                </Route>
+                </> 
+                ) : (
+                <Route exact path="/" element={<Auth />}>
+                </Route>
+                )}
+            </Routes>
+        </Router>
+    );
+};
+
+export default AppRouter;
