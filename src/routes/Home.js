9d55bf4971bd4fcf93b3ce8edaa240b4654a35b8
이예원 <yenn001004@gmail.com>
2022-04-21 18:14:01 +0900

    Getting the Yweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,20 +1,35 @@
+import React, { useEffect, useState } from "react";
 import { dbService } from "firebaseConfig";
-import React, { useState } from "react";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, getDocs, query } from "firebase/firestore";
+
 
 const Home = () => {
     const [yweet, setYweet] = useState("");
+    const [yweets, setYweets] = useState([]);
+    const getYweets = async () => {
+        const q = query(collection(dbService, "yweets"));
+        const querySnapshot = await getDocs(q);
+        const yweetObj = querySnapshot.docs.map((doc) => (
+            {
+                ...doc.data(),
+                id: doc.id,
+            }
+            
+        ));
+        setYweets(yweetObj);
+    };
+    useEffect(() => {
+        getYweets();
+    }, []);
+    
     const onSubmit = async (event) => {
         event.preventDefault();
-        try {
-            const docRef = await addDoc(collection(dbService, "yweets"), {
+            await addDoc(collection(dbService, "yweets"), {
                 yweet,
                 createdAt: Date.now(),
             });
-            console.log("Document written with ID: ", docRef.id);
-        } catch (error) {
-            console.error("Error adding document: ", error);
-        }
+            // console.log("Document written with ID: ", docRef.id);
+        
         setYweet("");
     };
     const onChange = (event) => {
@@ -23,12 +38,20 @@ const Home = () => {
         } = event;
         setYweet(value);
     };
+    console.log(yweets);
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input value={yweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
                 <input type="submit" value="Yweet" />
             </form>
+            <div>
+                {yweets.map((yweet) => (
+                    <div key={yweet.id}>
+                        <h4>{yweet.yweet}</h4>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
